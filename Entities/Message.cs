700e03318fc5c4a519dbd028737bb5b35a5661c0
Oier <oiersolabarrieta@gmail.com>
2022-03-19 13:16:14 +0100

    Creando la entidad Messages para devolver un mensaje + un objeto en las llamadas a las funciones.

---
+++ Entities/Message.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+
+namespace Entities
+{
+    public class Message
+    {
+        private DataView dv;
+        private String msg;
+
+        public Message(DataView dv, string msg)
+        {
+            this.dv = dv;
+            this.msg = msg;
+        }
+
+        public DataView Dv { get => dv; set => dv = value; }
+        public string Msg { get => msg; set => msg = value; }
+    }
+}
