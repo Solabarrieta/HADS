98b6ba6eed0573b8e50ac02b9576ea762d30ae7e
Oier <oiersolabarrieta@gmail.com>
2022-03-22 23:25:58 +0100

    Agregando la funcionalidad exportar documento xml

---
+++ Lab2/ExportarTareaXML.aspx.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+
+namespace Lab2
+{
+    public partial class WebForm12 : System.Web.UI.Page
+    {
+        BusinessLogic.Logic bl = new BusinessLogic.Logic();
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            Session["correo"] = "vadillo@ehu.es";
+        }
+
+        protected void ExportarBtn_Click(object sender, EventArgs e)
+        {
+
+            string asignatura = Session["asignatura"] as string;
+            DataSet dataset = bl.exportarTareas(asignatura);
+        }
+
+        protected void AsignaturasList_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            Session["asignatura"] = AsignaturasList.SelectedValue;
+        }
+    }
+}
